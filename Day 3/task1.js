8fdaf7462e70f8e3e5b46393b63407d7c62a3a26
UbaidUllah Bhutto <UbaidUllahBhutto@corp.contoursoftware.net>
2022-11-02 17:11:59 +0500

    palindrome checking

--- Day 3/task1.js
+++ Day 3/task1.js
@@ -1,10 +1,23 @@
-function check(){
-    let num1 = parseInt(document.getElementById('num1').value);
 
-    if(num1%2==0){
-        document.getElementById('big').innerHTML= num1 +" is even";
+
+function check(){
+    let str = document.getElementById('string').value;
+    let reverse="";
+    for(let i=str.length-1;i>=0;i--){
+
+        reverse+= str[i];
+
+    }
+    if(str==reverse){
+        document.getElementById('big').innerHTML= reverse + " is a palindrome";
+
     }
     else{
-        document.getElementById('big').innerHTML=num1 +" is odd";
+        document.getElementById('big').innerHTML= reverse + " is not a palindrome";
+        ;
     }
+
+   
+
+
 }
