4f898ebd3dbc2e4d0a7e89ff219026b30679f14b
UbaidUllah Bhutto <UbaidUllahBhutto@corp.contoursoftware.net>
2022-11-02 12:39:00 +0500

    learning JS

--- Day 2/task2.js
+++ Day 2/task2.js
@@ -2,7 +2,7 @@ function check(){
     input = document.getElementById("input1").value;
 
     if(input > 21){
-        document.getElementById('check').innerHTML = "you are allow to use mobile and laptop";
+        document.getElementById('check').innerText = "you are allow to use mobile and laptop";
 
     }
     else if (input >= 18) {
